6fc32fcf90e86b19c3a7361552f17cd18d1249b1
Adriano Hirata <adrianohirata@users.noreply.github.com>
2020-02-07 13:34:28 -0800

    Update dns-sync.sh

--- dns-sync/dns-sync.sh
+++ dns-sync/dns-sync.sh
@@ -6,7 +6,8 @@
 # Default-Start:     S
 # Default-Stop:
 # Short-Description: Synchronizes /etc/resolv.conf in WLS with Windows DNS
-# Description: Original script by Matthias Brooks https://gist.github.com/matthiassb/9c8162d2564777a70e3ae3cbee7d2e95
+# Description: Updated to keep codepage so it won't change the console font
+#              Original script by Matthias Brooks https://gist.github.com/matthiassb/9c8162d2564777a70e3ae3cbee7d2e95
 ### END INIT INFO
 
 PATH=/sbin:/bin:/mnt/c/WINDOWS/system32
@@ -18,22 +19,23 @@ PS=/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe
 do_start () {
         while true
         do
-	  oemcp=$(reg.exe query "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Nls\\CodePage" /v OEMCP | sed -n 3p | sed -e 's|\r||g' | grep -o '[[:digit:]]*')
-	  /mnt/c/WINDOWS/system32/chcp.com $oemcp > /dev/null
- 
+          oemcp=$(reg.exe query "HKLM\\SYSTEM\\CurrentControlSet\\Control\\Nls\\CodePage" /v OEMCP | sed -n 3p | sed -e 's|\r||g' | grep -o '[[:digit:]]*')
+          chcp.com $oemcp > /dev/null
+
           #Retrieve nameservers from via Powershell
           TEMPFILE=$(mktemp)
           $PS -Command "Get-DnsClientServerAddress -AddressFamily IPv4 | Select-Object -ExpandProperty ServerAddresses" > $TEMPFILE
           /usr/bin/awk '!x[$0]++' $TEMPFILE > $TEMPFILE.2
           IFS=$'\r\n' GLOBIGNORE='*' command eval  'UNIQUE_NAMESERVERS=($(cat $TEMPFILE.2))'
           rm -f $TEMPFILE $TEMPFILE.2
-          
+
           #Retrive search domains via powershell
           IFS=$'\r\n' GLOBIGNORE='*' command eval  'SEARCH_DOMAIN=($($PS -Command "Get-DnsClientGlobalSetting | Select-Object -ExpandProperty SuffixSearchList"))'
           UNIQUE_SEARCH_DOMAIN=($(/usr/bin/tr ' ' '\n' <<< "${SEARCH_DOMAIN[@]}" | /usr/bin/sort -u | /usr/bin/tr '\n' ' '))
 
-	  /mnt/c/WINDOWS/system32/chcp.com 65001 > /dev/null
-          
+          chcp.com $oemcp > /dev/null
+          # 65001
+
           #Modify /etc/resolv.conf
           touch /etc/resolv.conf
           sed -i '/nameserver/d' /etc/resolv.conf > /dev/null 2>&1 || true
@@ -41,7 +43,7 @@ do_start () {
 
           for i in "${UNIQUE_NAMESERVERS[@]}"
           do
-          	echo "nameserver ${i}" >> /etc/resolv.conf
+                echo "nameserver ${i}" >> /etc/resolv.conf
           done
           if [ ${#UNIQUE_SEARCH_DOMAIN[@]} -ne 0 ]; then
             echo "search ${UNIQUE_SEARCH_DOMAIN[@]}" >> /etc/resolv.conf
@@ -56,7 +58,7 @@ do_status () {
           echo "dns-sync is not running"
           return
         fi
-        
+
         if ps -p $PID > /dev/null
         then
            echo "dns-sync is running"
